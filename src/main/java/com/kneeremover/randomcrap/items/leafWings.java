f58184a77cde6c0d832a98643c0dbd4a519ec0e9
KneeRemover <96503761+KneeRemover@users.noreply.github.com>
2022-11-16 17:13:34 -0800

    e

--- src/main/java/com/kneeremover/randomcrap/items/leafWings.java
+++ src/main/java/com/kneeremover/randomcrap/items/leafWings.java
@@ -2,13 +2,13 @@ package com.kneeremover.randomcrap.items;
 
 
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
 
 public class leafWings extends Item {
     public leafWings(Properties properties) {
@@ -16,12 +16,11 @@ public class leafWings extends Item {
     }
 
     @Override
-    public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
+    public @NotNull ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
         ItemStack stack = player.getItemInHand(hand);
-        if (!world.isClientSide) {
-            ServerPlayerEntity splayer = (ServerPlayerEntity) player;
-            splayer.setDeltaMovement(Vector3d.directionFromRotation(player.getRotationVector()).normalize());
-            splayer.getMainHandItem().shrink(1);
+        if (world.isClientSide) {
+            player.setDeltaMovement(Vector3d.directionFromRotation(player.getRotationVector()).normalize().multiply(3, 3, 3));
+            stack.shrink(1);
         }
         return ActionResult.success(stack);
     }
